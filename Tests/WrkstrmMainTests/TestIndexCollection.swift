0347c2c19dc95e78f9b239cb9ff08fd5b657713e
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-19 20:18:23 -0800

    Add a test.

---
+++ Tests/WrkstrmMainTests/TestIndexCollection.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import WrkstrmMain
+
+final class IndexedCollectionTests: XCTestCase {
+
+  func testIndexedCollection() {
+    // Prepare a sample array and its indexed collection
+    let sampleArray = ["a", "b", "c"]
+    let indexedCollection = IndexedCollection(base: sampleArray)
+
+    // Expected results
+    let expectedResults = [(0, "a"), (1, "b"), (2, "c")]
+
+    // Test if indexedCollection contains the correct tuples of index and element
+    for (index, element) in indexedCollection {
+      XCTAssertEqual(index, expectedResults[index].0, "Index should match expected index.")
+      XCTAssertEqual(element, expectedResults[index].1, "Element should match expected element.")
+    }
+  }
+}
