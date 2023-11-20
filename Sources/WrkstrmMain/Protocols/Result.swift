cba4515be1030845eb245da2e92211d6756f51a8
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-19 19:15:01 -0800

    Update Result.swift

--- Sources/WrkstrmMain/Protocols/Result.swift
+++ Sources/WrkstrmMain/Protocols/Result.swift
@@ -1,5 +1,17 @@
+/// A value that represents either a success or a failure, including an associated value in each case.
+///
+/// This enum is used for operations that can result in either a successful outcome or an error.
+/// It encapsulates a successful outcome with its associated value or a failure with an error.
 public enum Result<Model> {
+  /// A success, storing a `Model` value.
+  ///
+  /// Use this case to represent a successful outcome of an operation and to store the result value.
+  /// - Parameter Model: The type of the value associated with a successful outcome.
   case success(Model)
 
+  /// A failure, storing an `Error`.
+  ///
+  /// Use this case to represent a failure in an operation and to store the associated error.
+  /// - Parameter Error: The error associated with the failure of the operation.
   case failure(Error)
 }
