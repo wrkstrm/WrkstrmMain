c53d532c2a5f104a73310134041a6abb5dd51afb
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-19 19:27:20 -0800

    Update Optional+Comparable.swift

--- Sources/WrkstrmMain/Extensions/Optional+Comparable.swift
+++ Sources/WrkstrmMain/Extensions/Optional+Comparable.swift
@@ -1,4 +1,20 @@
+/// Extension of `Optional` to conform to `Comparable` when the wrapped type is `Equatable` and `Comparable`.
+///
+/// This extension allows optional values to be compared using the less-than operator (`<`), provided that the wrapped type conforms to `Equatable` and `Comparable`.
+/// Comparison logic is as follows:
+/// - If both values are non-nil, their wrapped values are compared.
+/// - A nil value is always considered less than a non-nil value.
+/// - Two nil values are considered equal (not less than each other).
 extension Optional: Comparable where Wrapped: Equatable & Comparable {
+  /// Compares two optional values using the less-than operator.
+  ///
+  /// The comparison is based on the wrapped values of the optionals. If both optionals are non-nil,
+  /// their wrapped values are compared. An optional with a value is always greater than an optional without a value (nil).
+  ///
+  /// - Parameters:
+  ///   - lhs: An optional value to compare.
+  ///   - rhs: Another optional value to compare against.
+  /// - Returns: `true` if the value of `lhs` is less than the value of `rhs`; otherwise, `false`.
   public static func < (lhs: Wrapped?, rhs: Wrapped?) -> Bool {
     switch (lhs, rhs) {
       case let (l?, r?):
