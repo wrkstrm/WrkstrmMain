80c232648634636dcbe76e9263b798e20b4add18
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-19 19:22:14 -0800

    Update ClosedRange+Magnitude.swift

--- Sources/WrkstrmMain/Extensions/ClosedRange+Magnitude.swift
+++ Sources/WrkstrmMain/Extensions/ClosedRange+Magnitude.swift
@@ -1,7 +1,23 @@
-extension Range where Range.Bound: BinaryFloatingPoint & Comparable {
+/// An extension on `Range` for types conforming to `BinaryFloatingPoint` and `Comparable`.
+/// This extension adds a computed property to calculate the magnitude of a range.
+extension Range where Bound: BinaryFloatingPoint & Comparable {
+  /// The magnitude of the range.
+  ///
+  /// This computed property returns the difference between the upper and lower bounds of the range.
+  /// It is useful for determining the size or extent of the range when working with floating-point values.
+  ///
+  /// - Returns: The magnitude of the range as a `Bound` value, representing the distance between `upperBound` and `lowerBound`.
   public var magnitude: Bound { upperBound - lowerBound }
 }
 
-extension ClosedRange where ClosedRange.Bound: BinaryFloatingPoint & Comparable {
+/// An extension on `ClosedRange` for types conforming to `BinaryFloatingPoint` and `Comparable`.
+/// This extension adds a computed property to calculate the magnitude of a closed range.
+extension ClosedRange where Bound: BinaryFloatingPoint & Comparable {
+  /// The magnitude of the closed range.
+  ///
+  /// This computed property returns the difference between the upper and lower bounds of the closed range.
+  /// It is useful for determining the size or extent of the closed range when working with floating-point values.
+  ///
+  /// - Returns: The magnitude of the closed range as a `Bound` value, representing the distance between `upperBound` and `lowerBound`.
   public var magnitude: Bound { upperBound - lowerBound }
 }
