8ac8a22c62aaf8735cf624592c378c97b3528feb
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-12-20 15:03:51 -0800

    Move files and add tests.

--- Tests/WrkstrmMainTests/XCTestManifests.swift
+++ Tests/WrkstrmMainTests/XCTestManifests.swift
@@ -4,7 +4,11 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
   [
     testCase(StringKebabTests.allTests),
+    testCase(StringTests.allTests),
+    testCase(IndexedCollectionTests.allTests),
     testCase(BinaryTreeTests.allTests),
+    testCase(CollectionTests.allTests),
+    testCase(OptionalComparableTests.allTests),
     testCase(ListTests.allTests),
   ]
 }
