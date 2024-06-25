8ae8a91e796cd2351e0c2814fbf63f7743f8601c
Cristian A Monterroza <cmonterroza@laussatstudio.com>
2024-06-24 18:11:10 -0700

    Update line length issues.

--- Sources/WrkstrmMain/Extensions/RandomAccessCollection+IndexedCollection.swift
+++ Sources/WrkstrmMain/Extensions/RandomAccessCollection+IndexedCollection.swift
@@ -1,14 +1,15 @@
 /// An extension for `RandomAccessCollection`.
-/// This extension provides a method to convert any random access collection into an indexed collection.
+/// This extension provides a method to convert any random access collection into an indexed
+/// collection.
 extension RandomAccessCollection {
   /// Wraps the collection into an `IndexedCollection`.
   ///
-  /// This method provides indexed access to the elements of the collection. `IndexedCollection` is a
-  /// structure that pairs each element of the collection with its index, offering a convenient way to
-  /// enumerate a collection with index-aware operations.
+  /// This method provides indexed access to the elements of the collection. `IndexedCollection` is
+  /// a structure that pairs each element of the collection with its index, offering a convenient 
+  /// way to enumerate a collection with index-aware operations.
   ///
-  /// - Returns: An `IndexedCollection` instance that wraps the original collection, pairing its elements
-  /// with their respective indices.
+  /// - Returns: An `IndexedCollection` instance that wraps the original collection, pairing its
+  /// elements with their respective indices.
   func indexed() -> IndexedCollection<Self> {
     IndexedCollection(base: self)
   }
