77ffe64b213d6b6b7f93fc5ecabe10aee2a55753
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-07-30 02:51:55 -0700

    Simplify fuzzy array logic.

--- Sources/WrkstrmMain/JSON/Container+FuzzyArray.swift
+++ Sources/WrkstrmMain/JSON/Container+FuzzyArray.swift
@@ -11,57 +11,45 @@ extension KeyedDecodingContainer {
     _ type: T.Type = T.self,
     forKey key: Key
   ) throws -> [T]? where T: Decodable {
-    // Try decoding as array
-    if let array = try? decode([T].self, forKey: key) {
-      return array
+    guard contains(key) else {
+      throw DecodingError.dataCorruptedError(
+        forKey: key,
+        in: self,
+        debugDescription:
+          "Key not found in container. Key: \(key.stringValue) Type: (\(T.self))"
+      )
     }
-
-    // Try decoding as single value
-    if let single = try? decode(T.self, forKey: key) {
-      return [single]
-    }
-    
-    // Return nil for `null`
     if try decodeNil(forKey: key) {
       return nil
     }
-    
-    throw DecodingError.dataCorruptedError(
-      forKey: key,
-      in: self,
-      debugDescription:
-        "Expected null, single object, or array for key \(key.stringValue)"
-    )
+    do {
+      // Try decoding as array
+      return try decode([T].self, forKey: key)
+    } catch {
+      // Try decoding as single value
+      if let single = try? decodeObjectAllowingNullOrEmptyObject(T.self, forKey: key) {
+        return [single]
+      }
+      return nil
+    }
   }
-  
-  public func decodeAllowingNullOrEmptyObject<T>(
+
+  public func decodeObjectAllowingNullOrEmptyObject<T>(
     _ type: T.Type = T.self,
     forKey key: Key
   ) throws -> T? where T: Decodable {
     if try decodeNil(forKey: key) {
       return nil
     }
-    
-    // Try single value container (a single object)
-    if let single = try? decodeIfPresent(T.self, forKey: key) {
-      return single
-    }
-    
-    // If object, check if it's empty {}
-    if let nested = try? nestedContainer(
-      keyedBy: AnyCodingKey.self,
-      forKey: key
-    ),
-      nested.allKeys.isEmpty
-    {
+
+    do {
+      // Try decoding as an obect
+      return try decode(T.self, forKey: key)
+    } catch let potentialEmptyObjectError {
+      print("Expected null or empty object, or type (\(T.self)) for key \(key.stringValue)")
+      print("Ignoring Potential Empty object error. Confirm: \(potentialEmptyObjectError)")
+      // Try decoding as single value
       return nil
     }
-
-    throw DecodingError.dataCorruptedError(
-      forKey: key,
-      in: self,
-      debugDescription:
-        "Expected null, single object, or type (\(T.self)) for key \(key.stringValue)"
-    )
   }
 }
