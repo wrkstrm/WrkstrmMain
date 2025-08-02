405de27ab66bea54256527c48b4aeb0af0f797b7
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-02 00:41:06 -0700

    Update Container+FuzzyArray.swift

--- Sources/WrkstrmMain/JSON/Container+FuzzyArray.swift
+++ Sources/WrkstrmMain/JSON/Container+FuzzyArray.swift
@@ -27,29 +27,32 @@ extension KeyedDecodingContainer {
       return try decode([T].self, forKey: key)
     } catch {
       // Try decoding as single value
-      if let single = try? decodeObjectAllowingNullOrEmptyObject(T.self, forKey: key) {
+      if let single = try? decodeAllowingNullOrEmptyObject(T.self, forKey: key) {
         return [single]
       }
-      return nil
+      throw "Could not decode."
     }
   }
 
-  public func decodeObjectAllowingNullOrEmptyObject<T>(
+  /// Decodes a value for `key` that may be null, an empty object, or a fully-formed object, returning an optional.
+  ///
+  /// - parameter type: The type to decode.
+  /// - parameter key: The CodingKey to decode from.
+  /// - returns: `nil` if value is null or an empty object, or the decoded value of type `T`.
+  /// - throws: If decoding fails or the value is not of the expected shape/type.
+  public func decodeAllowingNullOrEmptyObject<T>(
     _ type: T.Type = T.self,
     forKey key: Key
   ) throws -> T? where T: Decodable {
     if try decodeNil(forKey: key) {
       return nil
     }
-
-    do {
-      // Try decoding as an obect
-      return try decode(T.self, forKey: key)
-    } catch let potentialEmptyObjectError {
-      print("Expected null or empty object, or type (\(T.self)) for key \(key.stringValue)")
-      print("Ignoring Potential Empty object error. Confirm: \(potentialEmptyObjectError)")
-      // Try decoding as single value
+    
+    if let _ = try? decode(JSON.EmptyCodableStruct.self, forKey: key) {
       return nil
     }
+
+    // Try decoding as an obect
+    return try decode(T.self, forKey: key)
   }
 }
