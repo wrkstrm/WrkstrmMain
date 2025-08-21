c4abb4bf452811ab0aba2487bc34711ae7b390db
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2025-08-20 23:34:14 -0700

    Exclude regional indicators from emoji set and fix ListTests

--- Tests/WrkstrmMainTests/ListTests.swift
+++ Tests/WrkstrmMainTests/ListTests.swift
@@ -32,12 +32,10 @@ struct ListTests {
   func testDoubleLinkedEqualityAndReferences() {
     // Ensures doubly linked nodes maintain correct equality and previous/next references,
     // guarding against corrupted links when traversing the list.
-    let head = List.double(previous: nil, current: 1, next: nil)
     var head = List.double(previous: nil, current: 1, next: nil)
-    var head = List.double(previous: nil, current: 1, next: nil)
-    let tail = List.double(previous: head, current: 2, next: nil)
+    var tail = List.double(previous: head, current: 2, next: nil)
     head = List.double(previous: nil, current: 1, next: tail)
-    head = List.double(previous: nil, current: 1, next: tail)
+    tail = List.double(previous: head, current: 2, next: nil)
     let tailCopy = List.double(previous: head, current: 2, next: nil)
 
     #expect(tail == tailCopy)
